1a1f5d69d04c330264add0d671a60ee0d63068f5
unknown <troskproductions@gmail.com>
2024-03-19 00:36:11 +0800

    Base app finished

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "firsthirewebscrapper": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "firsthirewebscrapperbbf75504": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "firstHireResources": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
